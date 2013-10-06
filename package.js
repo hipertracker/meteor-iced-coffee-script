2c60e1bd06799a3ed10421c7d9c6b59d0ef69845
Vita Smid <me@ze.phyr.us>
2013-10-06 14:03:57 +1100

    Updated build plugin registration

--- package.js
+++ package.js
@@ -1,38 +1,12 @@
 Package.describe({summary: "CoffeeScript with better steamline asynchronous control flow (http://maxtaco.github.io/coffee-script/)"});
 
-Npm.depends({
-  'iced-coffee-script': '1.6.3-e'
+Package._transitional_registerBuildPlugin({
+  name: "iced-coffee-script",
+  use: [],
+  sources: [
+    'plugin/compile-iced.js'
+  ],
+  npmDependencies: {'iced-coffee-script': '1.6.3-e'}
 });
 
-var iced_coffeescript_handler = function (bundle, source_path, serve_path, where) {
-  var fs = Npm.require('fs'),
-      path = Npm.require('path'),
-      iced = Npm.require('iced-coffee-script'),
-      serve_path = serve_path + '.js',
-      contents = fs.readFileSync(source_path);
-  options = {
-    bare: true, 
-    filename: source_path, 
-    literate: path.extname(source_path) === '.litcoffee'
-  };
-  try {
-    contents = iced.compile(contents.toString('utf8'), options);
-  } catch (e) {
-    return bundle.error(
-      source_path + ':' +
-      (e.location ? (e.location.first_line + ': ') : ' ') +
-      e.message
-    );
-  }
-  contents = new Buffer(contents);
-  bundle.add_resource({
-    type: 'js',
-    path: serve_path,
-    data: contents,
-    where: where
-  });
-};
-
-Package.register_extension('coffee', iced_coffeescript_handler);
-Package.register_extension('litcoffee', iced_coffeescript_handler);
 
---
+++ plugin/compile-iced.js
@@ -0,0 +1,33 @@
+var fs = Npm.require('fs');
+var path = Npm.require('path');
+var iced = Npm.require('iced-coffee-script');
+
+var handler = function (compileStep) {
+  var source = compileStep.read().toString('utf8');
+  var outputFile = compileStep.inputPath + ".js";
+
+  options = {
+    bare: true,
+    filename: compileStep.inputPath,
+    literate: path.extname(compileStep.inputPath) === '.litcoffee'
+  };
+
+  try {
+    var output = iced.compile(source, options);
+  } catch (e) {
+    throw new Error(
+      compileStep.inputPath + ':' +
+      (e.location ? (e.location.first_line + ': ') : ' ') +
+      e.message
+    );
+  }
+
+  compileStep.addJavaScript({
+    path: outputFile,
+    sourcePath: compileStep.inputPath,
+    data: output
+  });
+};
+
+Plugin.registerSourceHandler('coffee', handler);
+Plugin.registerSourceHandler('litcoffee', handler);
